40b4d9c685141336ed12e64dd9b293ac6568ead3
ZakariaMouhydine <mouhydinezakaria@gmail.com>
2022-01-23 22:56:04 +0000

    favicon + about page

--- script.js
+++ script.js
@@ -20,21 +20,3 @@ function displaySection (id) {
 
 
 
-//Get the button:
-mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 10px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.documentElement.scrollTop > 10) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.documentElement.scrollTop = 0;
-}
